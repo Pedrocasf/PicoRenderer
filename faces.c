a5e9bd4cf8dd3d27d09f02e38a06959446b03a67
Pedrocasf <pedrostarling2000@gmail.com>
2021-04-30 13:08:16 -0300

    Initial commit

---
+++ faces.c
@@ -0,0 +1,86 @@
+#include "faces.h"
+void normalize(Vec3 *p){
+    fixedpt w = fixedpt_sqrt(fixedpt_mul(p->x,p->x)+fixedpt_mul(p->y,p->y)+fixedpt_mul(p->z,p->z));
+    fixedpt_xdiv(p->x,w);
+    fixedpt_xdiv(p->y,w);
+    fixedpt_xdiv(p->z,w);
+}
+Vec3 sub(Vec3 a,Vec3 b){
+    Vec3 c;
+    c.x = a.x-b.x;
+    c.y = a.y-b.y;
+    c.z = a.z-b.z;
+    return c;
+};
+Vec3 cross(Vec3 a,Vec3 b){
+    Vec3 c;
+    c.x = fixedpt_mul(a.y,b.z) - fixedpt_mul(a.z,b.y);
+    c.y = fixedpt_mul(a.z,b.x) - fixedpt_mul(a.x,b.z);
+    c.z = fixedpt_mul(a.x,b.y) - fixedpt_mul(a.y,b.x);
+    return c;
+};
+Vec3 baricentro(Vec3i pts[3], Vec3i p){
+    Vec3 s[2] = {{0,0,0},{0,0,0}};
+    Vec3 r;
+    s[1].x = fixedpt_fromint(pts[2].y - pts[0].y);
+    s[1].y = fixedpt_fromint(pts[1].y - pts[0].y);
+    s[1].z = fixedpt_fromint(pts[0].y - p.y);
+
+    s[0].x = fixedpt_fromint(pts[2].x - pts[0].x);
+    s[0].y = fixedpt_fromint(pts[1].x - pts[0].x);
+    s[0].z = fixedpt_fromint(pts[0].x - p.x);
+    
+    Vec3 u = cross(s[0], s[1]);
+    if(fixedpt_abs(u.z)<0){
+        r.x = -FIXEDPT_ONE;
+        r.y = FIXEDPT_ONE;
+        r.z = FIXEDPT_ONE;
+    }else{
+        r.x = FIXEDPT_ONE-fixedpt_div(u.x+u.y, u.z);
+        r.y = fixedpt_div(u.y,u.z);
+        r.z = fixedpt_div(u.x,u.z);
+    };
+    return r;
+};
+fixedpt max(fixedpt x, fixedpt y){
+  return (x > y) ? x : y;
+};
+fixedpt min(fixedpt x, fixedpt y)
+{
+  return (x < y) ? x : y;
+}
+void triangulo(Vec3i pts[3] ,fixedpt* zbuffer,uint16_t*display, uint16_t cor){
+    Vec2i bboxmin, bboxmax, clamp;
+    bboxmin.x = 0x7FFFFFFF;
+    bboxmin.y = 0x7FFFFFFF;
+    bboxmax.x = -0x7FFFFFFF;;
+    bboxmax.y = -0x7FFFFFFF;;
+    clamp.x = WIDTH-1;
+    clamp.y = HEIGHT-1;
+    for (int i = 0;i<3;i++){
+        bboxmin.x  = max(0,min(bboxmin.x,pts[i].x));
+        bboxmax.x = min(clamp.x,max(bboxmax.x,pts[i].x));
+
+        bboxmin.y  = max(0,min(bboxmin.y,pts[i].y));
+        bboxmax.y = min(clamp.y,max(bboxmax.y,pts[i].y));
+    };
+    Vec3i p = {0,0,0};
+    while (p.x <= bboxmax.x){
+        p.y = bboxmin.y;
+        while (p.y <= bboxmax.y){
+            Vec3 bc_screen = baricentro(pts,p);
+            if (bc_screen.x>0 && bc_screen.y>0&& bc_screen.z>0){
+                p.z = 0;
+                p.z += fixedpt_mul(pts[0].z,bc_screen.x);
+                p.z += fixedpt_mul(pts[1].z,bc_screen.y);
+                p.z += fixedpt_mul(pts[2].z,bc_screen.z);
+                if(zbuffer[p.x + (p.y * WIDTH)]<p.z){
+                    zbuffer[p.x + (p.y * WIDTH)] = fixedpt_toint(p.z);
+                    display[p.x + (p.y * WIDTH)] = cor;
+                }
+            }
+            p.y += 1;
+        }
+        p.x += 1;
+    }
+};
---
+++ main.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "pico.h"
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/scanvideo.h"
+#include "hardware/irq.h"
+#include "pico/scanvideo/composable_scanline.h"
+#include "pico/sync.h"
+#include "textures.h"
+#include "fixedpt.h"
+#include "models.h"
+#include "faces.h"
+#define vga_mode vga_mode_160x120_60
+const uint Left = 19;
+const uint Right = 26;
+void render();
+void left();
+void right();
+static semaphore_t video_initted;
+static uint16_t display[WIDTH*HEIGHT] = {0};
+static fixedpt zbuffer[WIDTH*HEIGHT] = {0};
+static Vec3i screen_coords[3] = {{0,0,0},{0,0,0},{0,0,0}};
+static Vec3 v;
+static uint8_t i;
+static fixedpt intensity;
+static fixedpt scale = FIXEDPT_ONE;
+int main(void) {
+    set_sys_clock_khz(250000,true);
+    stdio_init_all();
+    sem_init(&video_initted, 0, 1);
+    multicore_launch_core1(render);
+    memset(zbuffer, -0x80000000, sizeof zbuffer);
+    sem_acquire_blocking(&video_initted);
+    printf("Program started");
+    while (true) {
+      int c = getchar_timeout_us(0);
+      switch (c) {
+        case ' ':
+        for(int j = 0;j<3;j++){
+          printf("x:%d\n", screen_coords[j].x);
+          printf("y:%d\n", screen_coords[j].y);
+          printf("z:%d\n", fixedpt_toint(screen_coords[j].z));
+          printf("i:%d\n", intensity);
+        }
+          break;
+      };
+      scanvideo_wait_for_vblank();
+      for (int y = 0;y<vga_mode.height;y++){
+        scanvideo_scanline_buffer_t *scanline_buffer = scanvideo_begin_scanline_generation(true);
+        uint16_t *p = (uint16_t *) scanline_buffer->data;
+        *p++ = COMPOSABLE_RAW_RUN;
+        *p++ = display[y* WIDTH];
+        *p++ = vga_mode.width;
+        memcpy(p, &display[y*WIDTH], sizeof(display[y])*WIDTH);
+        p[vga_mode.width+3]  = COMPOSABLE_EOL_SKIP_ALIGN;
+        p[vga_mode.width+4]  = 0;
+        scanline_buffer->data_used = (vga_mode.width + 6)>>1;
+        assert(scanline_buffer->data_used < scanline_buffer->data_max);
+        scanline_buffer->status = SCANLINE_OK;
+        scanvideo_end_scanline_generation(scanline_buffer);
+      }
+    }
+}
+void render(){
+  scanvideo_setup(&vga_mode);
+  scanvideo_timing_enable(true);
+  multicore_fifo_clear_irq();
+  stdio_init_all();
+  sem_release(&video_initted);
+  gpio_init(Left);
+  gpio_set_dir(Left, GPIO_IN);
+  gpio_pull_up(Left);
+  gpio_init(Right);
+  gpio_set_dir(Right, GPIO_IN);
+  gpio_pull_up(Right);
+  Vec3 l = {0,0,FIXEDPT_ONE};
+  while (1){
+    for(i = 0;i<rock_faces_len;i+=3){
+      Vec3 world_coords [3] = {{0,0,0},{0,0,0},{0,0,0}};
+      uint8_t face[3] = {rock_faces[i],rock_faces[i+1],rock_faces[i+2]};
+      for(uint8_t j = 0; j<3;j++){
+          v.x = fixedpt_mul(rock_vertices[(face[j]*3)], scale);
+          v.y = fixedpt_mul(rock_vertices[(face[j]*3)+1],scale);
+          v.z = rock_vertices[(face[j]*3)+2];
+          screen_coords[j] = (Vec3i){
+            fixedpt_toint(fixedpt_mul(v.x+FIXEDPT_ONE,fixedpt_fromint(WIDTH>>1))),
+            fixedpt_toint(fixedpt_mul(v.y+FIXEDPT_ONE,fixedpt_fromint(HEIGHT>>1))),
+            v.z
+          };
+          world_coords[j] = v;
+      }
+      Vec3 n = cross(sub(world_coords[0], world_coords[2]), sub(world_coords[1], world_coords[0]));
+      normalize(&n);
+      intensity = fixedpt_mul(n.x,l.x)+fixedpt_mul(n.y,l.y)+fixedpt_mul(n.z,l.z);
+      if(intensity > 0){
+        uint16_t r = fixedpt_toint(fixedpt_mul(intensity, fixedpt_fromint(31))); 
+        uint16_t g = fixedpt_toint(fixedpt_mul(intensity, fixedpt_fromint(63)))<<5; 
+        uint16_t b = fixedpt_toint(fixedpt_mul(intensity, fixedpt_fromint(31)))<<11; 
+        triangulo(screen_coords,zbuffer,display,b|g|r);
+      }
+    }
+    if(!gpio_get(Left)){
+      //memset(zbuffer, -0x80000000, sizeof zbuffer);
+      memset(display, 0, sizeof display);
+      scale+=FIXEDPT_ONE_HALF;
+    }
+    if(!gpio_get(Right)){
+      //memset(zbuffer, -0x80000000, sizeof zbuffer);
+      memset(display, 0, sizeof display);
+      scale-=FIXEDPT_ONE_HALF;
+    }
+  }
+}
